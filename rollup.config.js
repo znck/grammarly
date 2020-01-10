c15670af6890af08dcf6474e40c9bd210a246a5f
Rahul Kadyan <hi@znck.me>
2020-01-10 10:53:17 +0530

    fix: Out file should be server/index.js

--- rollup.config.js
+++ rollup.config.js
@@ -28,7 +28,7 @@ export default [
     input: 'src/server/index.ts',
     output: {
       format: 'cjs',
-      file: 'out/server.js',
+      file: 'out/server/index.js',
       sourcemap: true,
     },
     plugins: [node({ preferBuiltins: true }), cjs(), ts({ tsconfig: 'tsconfig.build.json' })],
