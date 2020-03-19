78fc7ab924daa6fc0f3ff165cbabb77c028fd41f
Rahul Kadyan <hi@znck.me>
2020-03-20 02:26:09 +0530

    chore: add lint-staged and prettier

--- src/shared/inMemoryKeyChain.ts
+++ src/shared/inMemoryKeyChain.ts
@@ -1,24 +1,28 @@
-import { KeyChain } from './credentialsStore'
+import { KeyChain } from "./credentialsStore";
 
-export const inMemoryKeyChain =(accounts: Record<string, string>) : KeyChain => {
-    let keyChainAccounts = accounts
+export const inMemoryKeyChain = (
+  accounts: Record<string, string>
+): KeyChain => {
+  let keyChainAccounts = accounts;
 
-    return {
-        getPassword: async (service: string, account: string) => {
-           return keyChainAccounts[account]
-        },
-        setPassword: async (service: string, account: string, password: string) => {
-            keyChainAccounts[account] = password
-        },
-        deletePassword: async (service: string, account: string) => {
-           delete keyChainAccounts[account]
-           return true
-        },
-        findCredentials: async (service: string): Promise<Array<{account: string, password: string}>> => {
-           return Object.entries(keyChainAccounts).map(([key, val]) => ({
-               account: key,
-               password: val
-           }))
-        }
-    } as KeyChain
-}
+  return {
+    getPassword: async (service: string, account: string) => {
+      return keyChainAccounts[account];
+    },
+    setPassword: async (service: string, account: string, password: string) => {
+      keyChainAccounts[account] = password;
+    },
+    deletePassword: async (service: string, account: string) => {
+      delete keyChainAccounts[account];
+      return true;
+    },
+    findCredentials: async (
+      service: string
+    ): Promise<Array<{ account: string; password: string }>> => {
+      return Object.entries(keyChainAccounts).map(([key, val]) => ({
+        account: key,
+        password: val
+      }));
+    }
+  } as KeyChain;
+};
