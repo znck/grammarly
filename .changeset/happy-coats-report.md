2de7e7966d4a3e9249792576820efdda600cfd3b
Rahul Kadyan <hi@znck.me>
2022-05-11 13:10:16 -0700

    Support connecting Grammarly account on github.dev (#246)
    * Use IndexDB to persist data in localStorage
    
    * restart on error
    
    * Use vscode.env.asExternalUri() to get a redirect URI in web extension
    
    * add changeset
    
    * use a netlify function to handle redirect using state
    
    Co-authored-by: Rahul Kadyan <rahul.kadyan@grammarly.com>

---
+++ .changeset/happy-coats-report.md
@@ -0,0 +1,6 @@
+---
+'grammarly': minor
+'grammarly-languageserver': patch
+---
+
+Support for connected Grammarly account in web extension (https://github.dev and https://vscode.dev)
--- packages/grammarly-languageserver/src/VirtualStorage.ts
+++ packages/grammarly-languageserver/src/VirtualStorage.ts
@@ -1,3 +1,5 @@
+import { createStore, get, set, keys, del, delMany } from 'idb-keyval'
+
 export class VirtualStorage {
   public items = new Map<string, string>()
 
@@ -22,6 +24,46 @@ export class VirtualStorage {
   }
 
   setItem(key: string, value: string): void {
+    this.items.set(key, value)
+  }
+}
+
+export class IDBStorage extends VirtualStorage {
+  private store = createStore('grammarly-languageserver', 'localStorage')
+
+  async load(): Promise<void> {
+    for (const key of await keys<string>(this.store)) {
+      const value = await get<string>(key, this.store)
+      if (value != null) {
+        this.items.set(key, value)
+      }
+    }
+  }
+
+  clear(): void {
+    delMany(Array.from(this.items.keys()), this.store)
+    super.clear()
+  }
+
+  getItem(key: string): string | null {
+    get(key, this.store).then((value) => {
+      if (value != null) {
+        this.items.set(key, value)
+      } else {
+        this.items.delete(key)
+      }
+    })
+
+    return super.getItem(key)
+  }
+
+  removeItem(key: string): void {
+    del(key, this.store)
+    this.items.delete(key)
+  }
+
+  setItem(key: string, value: string): void {
+    set(key, value, this.store)
     this.items.set(key, value)
   }
 }
